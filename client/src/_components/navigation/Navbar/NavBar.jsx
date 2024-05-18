6b19bd07b34ff961788d5dd7c2af19fb7ae73b17
ikarthik21 <karthikeyakallapu@gmail.com>
2024-05-19 00:10:50 +0530

    added jwt token based authentication

--- client/src/_components/navigation/Navbar/NavBar.jsx
+++ client/src/_components/navigation/Navbar/NavBar.jsx
@@ -1,17 +1,19 @@
 import {Button} from "@/components/ui/button"
-
+import useAuthStore from "@/store/use-auth.js";
 
 // eslint-disable-next-line react/prop-types
 const NavBar = ({openModal}) => {
-    return (
-        <nav
-            className="flex h-[70px] text-white bg-[#18181b] fixed top-0  left-0 p-2  lg:px-4 items-center w-full justify-between   border-b border-[#2D2E35]">
-            <h1>Navbar</h1>
-            <Button variant="outline" onClick={openModal}>
-                Login
-            </Button>
-        </nav>
-    )
+    const {isAuthenticated, removeAccessToken} = useAuthStore((state) => state);
+
+    return (<nav
+        className="flex h-[70px] text-white bg-[#18181b] fixed top-0  left-0 p-2  lg:px-4 items-center w-full justify-between   border-b border-[#2D2E35]">
+        <h1>Navbar</h1>
+        {isAuthenticated ? <Button variant="outline" onClick={removeAccessToken}>
+            Logout
+        < /Button> : <Button variant="outline" onClick={openModal}>
+            Login
+        </Button>}
+    </nav>)
 }
 
 export default NavBar
--- client/src/store/user-auth.js
+++
@@ -1,8 +0,0 @@
-import { create } from 'zustand';
-
-
-export const useAuth = create((set) => ({
-    user: {},
-    onExpand: () => set(() => ({ collapse: false })),
-    onCollapse: () => set(() => ({ collapse: true })),
-}));
