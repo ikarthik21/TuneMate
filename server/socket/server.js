752934eb229ca649f2e861d15ea392daad17999f
ikarthik21 <karthikeyakallapu@gmail.com>
2024-12-06 19:49:11 +0530

    corrected the ws server config

--- server/socket/server.js
+++ server/socket/server.js
@@ -1,14 +1,20 @@
-import {startWebSocketServer} from './services/websocketService.js';
-import express from 'express';
-import router from './routes/routes.js';
+import { startWebSocketServer } from "./services/websocketService.js";
+import express from "express";
+import router from "./routes/routes.js";
+import http from "http"; // Import HTTP server
 
-const PORT                  = process.env.PORT || 4100;
-const  server               = express();
+const PORT = process.env.PORT || 4100;
+const app = express();
 
-server.use(express.json());
-server.use(router);
+// Middleware and routes
+app.use(express.json());
+app.use(router);
+
+const server = http.createServer(app);
+
 startWebSocketServer(server);
 
 server.listen(PORT, () => {
-    console.log(`WebSocket server is listening on ws://localhost:${PORT}`);
+  console.log(`Server is listening on http://localhost:${PORT}`);
+  console.log(`WebSocket server is listening on ws://localhost:${PORT}`);
 });
