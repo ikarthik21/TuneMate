d4b2a25c6bcae5e57f11401b4ae3ab344df670f6
ikarthik21 <karthikeyakallapu@gmail.com>
2024-12-07 16:09:25 +0530

    configured seperate collection for favorites and history

--- client/src/service/api/api.js
+++ client/src/service/api/api.js
@@ -1,167 +1,190 @@
-import axios from 'axios';
-import Cookies from 'js-cookie';
-import {ENDPOINTS} from "@/service/endpoints/API_ENDPOINTS.js";
+import axios from "axios";
+import Cookies from "js-cookie";
+import { ENDPOINTS } from "@/service/endpoints/API_ENDPOINTS.js";
 
 const baseURL = import.meta.env.VITE_BACKEND_URL;
-const token = Cookies.get('accessToken');
+const token = Cookies.get("accessToken");
 
 export const tuneMateClient = axios.create({
-    baseURL, headers: {
-        'Authorization': token ? `Bearer ${token}` : undefined
-    }
+  baseURL,
+  headers: {
+    Authorization: token ? `Bearer ${token}` : undefined
+  }
 });
 
-
 class TuneMateService {
-
-    addSongToHistory = async (song) => {
-        try {
-            const response = await tuneMateClient.post(ENDPOINTS.addSongToHistory, song);
-            return response.data;
-        } catch (err) {
-            return err;
-        }
-
+  addSongToHistory = async (song) => {
+    try {
+      const response = await tuneMateClient.post(
+        ENDPOINTS.addSongToHistory,
+        song
+      );
+      return response.data;
+    } catch (err) {
+      return err;
     }
+  };
 
-    getPlaylists = async () => {
-        try {
-            const response = await tuneMateClient.get(ENDPOINTS.getPlaylists);
-            return response.data.playlists;
-        } catch (err) {
-            return err;
-        }
+  getPlaylists = async () => {
+    try {
+      const response = await tuneMateClient.get(ENDPOINTS.getPlaylists);
+      return response.data.playlists;
+    } catch (err) {
+      return err;
     }
+  };
 
-    loginUser = async (data) => {
-        try {
-            const response = await tuneMateClient.post(ENDPOINTS.login, data);
-            return response.data;
-        } catch (err) {
-            return err;
-        }
+  loginUser = async (data) => {
+    try {
+      const response = await tuneMateClient.post(ENDPOINTS.login, data);
+      return response.data;
+    } catch (err) {
+      return err;
     }
+  };
 
-    registerUser = async (data) => {
-        try {
-            const response = await tuneMateClient.post(ENDPOINTS.register, data);
-            return response.data;
-        } catch (err) {
-            return err;
-        }
-    };
-
-    ManageSongInFavorites = async (id) => {
-        try {
-            const response = await tuneMateClient.post(ENDPOINTS.ManageSongInFavorites, {id});
-            return response.data;
-        } catch (err) {
-            return err;
-        }
+  registerUser = async (data) => {
+    try {
+      const response = await tuneMateClient.post(ENDPOINTS.register, data);
+      return response.data;
+    } catch (err) {
+      return err;
     }
+  };
 
-    getFavorites = async () => {
-        try {
-            const response = await tuneMateClient.get(ENDPOINTS.favorites);
-            return response.data.favorites;
-        } catch (err) {
-            return err;
-        }
+  ManageSongInFavorites = async (id) => {
+    try {
+      const response = await tuneMateClient.post(
+        ENDPOINTS.ManageSongInFavorites,
+        { id }
+      );
+      return response.data;
+    } catch (err) {
+      return err;
     }
+  };
 
-    updatePlayerState = async (state) => {
-        try {
-            const response = await tuneMateClient.post(ENDPOINTS.updatePlayerState, {state});
-            return response.data;
-        } catch (err) {
-            return err;
-        }
+  getFavorites = async () => {
+    try {
+      const response = await tuneMateClient.get(ENDPOINTS.favorites);
+      return response.data.favorites;
+    } catch (err) {
+      return err;
     }
+  };
 
-    getPlayerState = async () => {
-        try {
-            const response = await tuneMateClient.get(ENDPOINTS.loadPlayerState);
-            return response.data;
-        } catch (err) {
-            return err;
-        }
+  updatePlayerState = async (state) => {
+    try {
+      const response = await tuneMateClient.post(ENDPOINTS.updatePlayerState, {
+        state
+      });
+      return response.data;
+    } catch (err) {
+      return err;
     }
-    getUserSongHistory = async () => {
-        try {
-            const response = await tuneMateClient.get(ENDPOINTS.getUserSongHistory);
-            return response.data.history;
-        } catch (err) {
-            return err;
-        }
+  };
 
+  getPlayerState = async () => {
+    try {
+      const response = await tuneMateClient.get(ENDPOINTS.loadPlayerState);
+      return response.data;
+    } catch (err) {
+      return err;
     }
-
-    createNewPlaylist = async (data, role) => {
-        try {
-            if (role === 'admin') {
-                const response = await tuneMateClient.post(ENDPOINTS.createRecommended, {playlist: data});
-                return response.data;
-            }
-            const response = await tuneMateClient.post(ENDPOINTS.createNewPlaylist, {playlist: data});
-            return response.data;
-        } catch (err) {
-            return err;
-        }
+  };
+  getUserSongHistory = async () => {
+    try {
+      const response = await tuneMateClient.get(ENDPOINTS.getUserSongHistory);
+      return response.data.history;
+    } catch (err) {
+      return err;
     }
+  };
 
-    saveSongInPlaylist = async (data, role) => {
-        try {
-            if (role === 'admin') {
-                const response = await tuneMateClient.post(ENDPOINTS.addSongToRecommended, data);
-                return response.data;
-            }
-            const response = await tuneMateClient.post(ENDPOINTS.saveSongInPlaylist, data);
-            return response.data;
-        } catch (err) {
-            return err;
-        }
+  createNewPlaylist = async (data, role) => {
+    try {
+      if (role === "admin") {
+        const response = await tuneMateClient.post(
+          ENDPOINTS.createRecommended,
+          { playlist: data }
+        );
+        return response.data;
+      }
+      const response = await tuneMateClient.post(ENDPOINTS.createNewPlaylist, {
+        playlist: data
+      });
+      return response.data;
+    } catch (err) {
+      return err;
     }
+  };
 
-    removeSongFromPlaylist = async (data, role) => {
-        try {
-            if (role === 'admin') {
-                const response = await tuneMateClient.post(ENDPOINTS.removeSongFromRecommended, data);
-                return response.data;
-            }
-            const response = await tuneMateClient.post(ENDPOINTS.removeSongFromPlaylist, data);
-            return response.data;
-        } catch (err) {
-            return err;
-        }
+  saveSongInPlaylist = async (data, role) => {
+    try {
+      if (role === "admin") {
+        const response = await tuneMateClient.post(
+          ENDPOINTS.addSongToRecommended,
+          data
+        );
+        return response.data;
+      }
+      const response = await tuneMateClient.post(
+        ENDPOINTS.saveSongInPlaylist,
+        data
+      );
+      return response.data;
+    } catch (err) {
+      return err;
     }
+  };
 
-    getUserPlaylist = async (id) => {
-        try {
-            const response = await tuneMateClient.get(ENDPOINTS.playlist(id));
-            return response.data.playlist[0];
-        } catch (err) {
-            return err;
-        }
+  removeSongFromPlaylist = async (data, role) => {
+    try {
+      if (role === "admin") {
+        const response = await tuneMateClient.post(
+          ENDPOINTS.removeSongFromRecommended,
+          data
+        );
+        return response.data;
+      }
+      const response = await tuneMateClient.post(
+        ENDPOINTS.removeSongFromPlaylist,
+        data
+      );
+      return response.data;
+    } catch (err) {
+      return err;
     }
+  };
 
-    getTuneMateRecommended = async () => {
-        try {
-            const response = await tuneMateClient.get(ENDPOINTS.recommended);
-            return response.data;
-        } catch (err) {
-            return err;
-        }
+  getUserPlaylist = async (id) => {
+    try {
+      const response = await tuneMateClient.get(ENDPOINTS.playlist(id));
+      return response.data.playlist[0];
+    } catch (err) {
+      return err;
     }
+  };
 
-    getRecommendedPlaylist = async (id) => {
-        try {
-            const response = await tuneMateClient.get(ENDPOINTS.recommendedPlaylist(id));
-            return response.data.playlist[0];
-        } catch (err) {
-            return err;
-        }
+  getTuneMateRecommended = async () => {
+    try {
+      const response = await tuneMateClient.get(ENDPOINTS.recommended);
+      return response.data;
+    } catch (err) {
+      return err;
     }
+  };
 
+  getRecommendedPlaylist = async (id) => {
+    try {
+      const response = await tuneMateClient.get(
+        ENDPOINTS.recommendedPlaylist(id)
+      );
+      return response.data.playlist[0];
+    } catch (err) {
+      return err;
+    }
+  };
 }
 
 const tuneMateInstance = new TuneMateService();
